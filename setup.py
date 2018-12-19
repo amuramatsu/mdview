70465c6550a9cae2401ade01529f7ecc94b2fefb
MURAMATSU Atsushi <amura@tomato.sakura.ne.jp>
2018-12-19 10:49:32 +0900

    fix for python3

--- setup.py
+++ setup.py
@@ -15,8 +15,9 @@ def get_version(filename):
     with open(filename) as fp:
         for line in fp:
             if line.startswith('__version__'):
-                exec(line,globals())
-                return __version__
+                namespace = {}
+                exec(line, namespace)
+                return namespace['__version__']
 
 setup(
     name='mdview',
